25518c2ea9a3f5c0a21ad59a92dc103874bc5762
musman <m_usman010@yahoo.com>
2026-05-16 13:53:55 +0100

    Modified buildroot, runqemu.sh

--- runqemu.sh
+++ runqemu.sh
@@ -4,12 +4,29 @@
 #Author: Siddhant Jajoo.
 
 
+#This old qemu configuration from assignment 4 also doesn´t work here
+#qemu-system-aarch64 \
+#    -M virt  \
+#    -cpu cortex-a53 -nographic -smp 1 \
+#    -kernel buildroot/output/images/Image \
+#    -append "rootwait root=/dev/vda console=ttyAMA0" \
+#    -netdev user,id=eth0,hostfwd=tcp::10022-:22 \
+#    -device virtio-net-device,netdev=eth0 \
+#    -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
+#    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+
+#Qemu New Configuration
 qemu-system-aarch64 \
-    -M virt  \
-    -cpu cortex-a53 -nographic -smp 1 \
+    -M virt \
+    -cpu cortex-a53 \
+    -nographic \
+    -smp 1 \
     -kernel buildroot/output/images/Image \
     -append "rootwait root=/dev/vda console=ttyAMA0" \
-    -netdev user,id=eth0,hostfwd=tcp::9000-:9000,hostfwd=tcp::10022-:22 \
-    -device virtio-net-device,netdev=eth0 \
+    -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
+    -device virtio-net-pci,netdev=eth0 \
     -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
-    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+    -device virtio-blk-device,drive=hd0 \
+    -device virtio-rng-pci
+
+
